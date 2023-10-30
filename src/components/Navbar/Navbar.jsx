ef7b1814859b5de0edc93d9035bf24e1145d1663
Rajin Sakha <rajinsakha07@gmail.com>
2023-10-30 18:09:59 +0545

    hero and service responsive done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,51 @@
-import React from 'react'
-import './Navbar.css';
-import { logo } from '../../assets';
-import { navLinks } from '../../constants';
+import React, { useState } from "react";
+import "./Navbar.css";
+import { logo } from "../../assets";
+import { navLinks } from "../../constants";
+import {MdMenu} from 'react-icons/md';
+import {IoMdClose} from 'react-icons/io'
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
-    <div className='app__navbar'>
+    <nav className="app__navbar">
       <a href="#">
         <img src={logo} alt="logo" width={114.912} height={33.994} />
       </a>
 
-      <ul className='app__navbar_links'>
-        {navLinks.map((link)=>(
-            <li key={link.label}>
-                <a href={link.href}>{link.label}</a>
-            </li>
+      <ul className="app__navbar_links">
+        {navLinks.map((link) => (
+          <li key={link.label}>
+            <a href={link.href}>{link.label}</a>
+          </li>
         ))}
       </ul>
 
-       <div className="app__navbar_account">
-        <a href='#'>Login</a>
+      <div className="app__navbar_account">
+        <a href="#">Login</a>
         <a href="#">Signup</a>
-        </div>     
+      </div>
 
-    </div>
-  )
-}
+      <div className="app__navbar_smallscreen">
+          <MdMenu fontSize={24} onClick={()=>setToggleMenu(true)} />
 
-export default Navbar
+        
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay">
+            <ul className="app__navbar_smallscreen_links">
+              {navLinks.map((link) => (
+                <li key={link.label}>
+                  <a href={link.href}>{link.label}</a>
+                </li>
+              ))}
+            </ul>
+            <IoMdClose fontSize={28} onClick={()=>setToggleMenu(false)} />
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/containers/Services/Services.jsx
+++ src/containers/Services/Services.jsx
@@ -14,7 +14,6 @@ const Services = () => {
         <ServiceItem key={service.title} {...service} />
       ))}
   
-    
     </div>
     </main>
   )
